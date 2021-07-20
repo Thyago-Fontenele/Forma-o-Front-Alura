1b806915d1f830f066d62c1093bd18f41880773c
Thyago Fontenele <thyagofontenele@id.uff.br>
2021-07-19 22:55:49 -0300

    começando js orientado a objetos

---
+++ PraticasJs/Array.js
@@ -0,0 +1,13 @@
+const ListaDeMelhoresDoFutebol = new Array(
+    'Pelé',
+    'Maradona',
+    'Garrinxa',
+    'Romário',
+    'Ronaldo',
+    'Ronaldinho',
+    'Cristiano Ronaldo',
+    'Messi'
+);
+
+console.log("O melhor da história foi: " + ListaDeMelhoresDoFutebol[1]);
+console.log("O melhor do século 21 será: "+ ListaDeMelhoresDoFutebol[6]);
---
+++ PraticasJs/objetos.js
@@ -0,0 +1,24 @@
+var lanchonete ={
+    salgados: [
+        "italiano",
+        "hamburger",
+        "enroladinho",
+        'salsicha'
+    ],
+    bebidas: [
+        'agua',
+        'guarana',
+        'coca',
+        'guaravita',
+        
+    ],
+    doces: [
+        'brigadeiro',
+        'bolo de morango',
+        'açai',
+        'pudim'
+    ]
+};
+for(var i of lanchonete.bebidas){
+    console.log(i);
+}
